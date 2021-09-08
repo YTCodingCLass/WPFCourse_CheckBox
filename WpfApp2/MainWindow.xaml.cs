d9da0a2c53dd5de51a904d49c2bab3c4a057d9cc
YTCodingClass <familiekamhieh@gmail.com>
2021-09-08 22:28:08 +0200

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if(cb_License_1.IsChecked == true && cb_License_2.IsChecked == true)
+            {
+                btn_Register.IsEnabled = true;
+            }
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if(cb_License_1.IsChecked == false || cb_License_2.IsChecked == false)
+            {
+                btn_Register.IsEnabled = false;
+            }
+        }
+
+
+    }
+}
